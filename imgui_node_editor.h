ba1e9a2490f8023321a98a941f0fb6a05f2da421
Kevin Balke <fughilli@gmail.com>
2022-04-19 11:37:13 -0700

    Fix includes to use the normal include path

--- imgui_node_editor.h
+++ imgui_node_editor.h
@@ -13,7 +13,7 @@
 
 
 //------------------------------------------------------------------------------
-# include <imgui.h>
+# include "imgui.h"
 # include <cstdint> // std::uintXX_t
 # include <utility> // std::move
 
